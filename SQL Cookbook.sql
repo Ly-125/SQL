cc002be834e41da8c0b3912f1a91cf33fca7f2a9
小渣渣 <1874620567@qq.com>
2020-05-24 22:42:18 +0800

    Update SQL Cookbook.sql

--- SQL Cookbook.sql
+++ SQL Cookbook.sql
@@ -4,3 +4,8 @@ create table dept(
 DEPTON int primary key,
 DNAME varchar(50) not null,
 LOC varchar(50) not null)
+insert into dept values 
+('10','ACCOUNTING','NEW YORK'),
+('20','RESEARCH','DALLAS'),
+('30','SALES','CHICAGO'),
+('40','OPERATIONS','BOSTON');
